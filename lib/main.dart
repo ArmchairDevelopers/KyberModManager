83c068cb7e990a132dd67398bad96a7218854780
liam <47498574+7reax@users.noreply.github.com>
2022-04-26 13:48:45 +0200

    minor ui fix

--- lib/main.dart
+++ lib/main.dart
@@ -113,7 +113,7 @@ class _AppState extends State<App> {
       color: SystemTheme.accentColor.accent.toAccentColor(),
       theme: ThemeData(
         accentColor: SystemTheme.accentColor.accent.toAccentColor(),
-        cardColor: Colors.white.withOpacity(.025),
+        cardColor: micaSupported ? Colors.white.withOpacity(.025) : null,
         brightness: Brightness.dark,
       ),
       localizationsDelegates: [
