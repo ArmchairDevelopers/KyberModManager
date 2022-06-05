26646c739555b065a0ef76e039cbf7c776d546fa
liam <47498574+7reax@users.noreply.github.com>
2022-06-06 00:41:17 +0200

    remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -57,12 +57,6 @@ void main() async {
     runApp(LocalizedApp(delegate, const App()));
     Logger.root.info('Started in ${DateTime.now().difference(started).inMilliseconds}ms');
   }, (exception, stackTrace) async {
-    // await FlutterPlatformAlert.showAlert(
-    //   windowTitle: 'Error',
-    //   text: 'An unknown error occurred:\n$exception\n$stackTrace',
-    //   alertStyle: AlertButtonStyle.ok,
-    //   iconStyle: IconStyle.error,
-    // );
     Logger.root.severe('$exception\n$stackTrace');
     await Sentry.captureException(exception, stackTrace: stackTrace);
   });
