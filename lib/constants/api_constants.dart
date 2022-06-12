451ed445039e85c1a667ec8a60fc637043c271d1
liam <47498574+7reax@users.noreply.github.com>
2022-06-12 15:32:58 +0200

    change api host

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,3 +1,3 @@
 const String KYBER_API_BASE_URL = "https://kyber.gg/api";
-const String BACKEND_API_BASE_URL = "http://localhost:3000/api";
+const String BACKEND_API_BASE_URL = "https://kyber.reax.at/api";
 const String KYBER_DLL_LINK = "$KYBER_API_BASE_URL/downloads/distributions/stable/dll";
