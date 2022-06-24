6463ac2ec5a4908e1929606606260fbb6078d3d6
liam <47498574+7reax@users.noreply.github.com>
2022-06-24 23:43:09 +0200

    check for invalid bf2 settings

---
+++ lib/screens/dialogs/battlefront_options_dialog.dart
@@ -0,0 +1,63 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:kyber_mod_manager/utils/battlefront_options.dart';
+
+class BattlefrontOptionsDialog extends StatefulWidget {
+  const BattlefrontOptionsDialog({Key? key}) : super(key: key);
+
+  @override
+  State<BattlefrontOptionsDialog> createState() => _BattlefrontOptionsDialogState();
+}
+
+class _BattlefrontOptionsDialogState extends State<BattlefrontOptionsDialog> {
+  BattlefrontProfileOptions? options;
+
+  @override
+  void initState() {
+    BattlefrontOptions.getOptions().then((value) => setState(() => options = value));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ContentDialog(
+      backgroundDismiss: false,
+      constraints: const BoxConstraints(maxWidth: 700, maxHeight: 400),
+      title: const Text("Battlefront Settings"),
+      actions: [
+        Button(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('Skip'),
+        ),
+        FilledButton(
+          onPressed: () async {
+            await BattlefrontOptions.setConfig();
+            if (!mounted) return;
+            Navigator.of(context).pop();
+          },
+          child: const Text('BF2 Settings'),
+        ),
+      ],
+      content: Center(
+        child: Column(
+          children: [
+            const Text('text'),
+            const SizedBox(
+              height: 20,
+            ),
+            if (options?.enableDx12 ?? false)
+              const Text(
+                'dx12',
+              ),
+            if (options?.fullscreenEnabled ?? false)
+              const Text(
+                'fullscreen',
+                style: TextStyle(fontSize: 14),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/battlefront_options.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
+class BattlefrontOptions {
+  static Future<BattlefrontProfileOptions?> getOptions() async {
+    String config = await BattlefrontOptions._getConfig();
+
+    if (config.isEmpty) {
+      return null;
+    }
+
+    return BattlefrontProfileOptions(
+      enableDx12: config.contains('GstRender.EnableDx12 1'),
+      fullscreenEnabled: config.contains('GstRender.FullscreenEnabled 1'),
+    );
+  }
+
+  static Future<void> setConfig() async {
+    String config = await _getConfig();
+    String configPath = await _getConfigPath();
+    var values = [
+      ['GstRender.EnableDx12 1', 'GstRender.EnableDx12 0'],
+      ['GstRender.FullscreenEnabled 1', 'GstRender.FullscreenEnabled 0'],
+      ['GstRender.FullscreenMode 0', 'GstRender.FullscreenMode 1'],
+      ['GstRender.FullscreenMode 0', 'GstRender.FullscreenMode 1'],
+    ];
+    values.forEach((value) => config.replaceAll(value[0], value[1]));
+    await File(configPath).writeAsString(config);
+  }
+
+  static Future<String> _getConfig() async {
+    String configPath = await _getConfigPath();
+    return File(configPath).readAsString();
+  }
+
+  static Future<String> _getConfigPath() async {
+    Directory docs = await getApplicationDocumentsDirectory();
+    if (!Directory('${docs.path}/STAR WARS Battlefront II').existsSync()) {
+      return '';
+    }
+
+    return '${docs.path}\\STAR WARS Battlefront II\\settings\\ProfileOptions_profile';
+  }
+}
+
+class BattlefrontProfileOptions {
+  bool enableDx12;
+  bool fullscreenEnabled;
+
+  BattlefrontProfileOptions({required this.enableDx12, required this.fullscreenEnabled});
+}
