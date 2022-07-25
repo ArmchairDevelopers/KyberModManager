2c2635fba31812cd61617bb533fd0ec0d60535f8
liam <47498574+7reax@users.noreply.github.com>
2022-07-25 17:43:51 +0200

    add button to manually select bf2

--- lib/screens/errors/battlefront_not_installed.dart
+++ lib/screens/errors/battlefront_not_installed.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:kyber_mod_manager/main.dart';
 import 'package:kyber_mod_manager/utils/helpers/origin_helper.dart';
 import 'package:kyber_mod_manager/utils/services/notification_service.dart';
 
@@ -21,17 +25,39 @@ class _BattlefrontNotFoundState extends State<BattlefrontNotFound> {
           const SizedBox(
             height: 16,
           ),
-          FilledButton(
-            child: const Text('Try again'),
-            onPressed: () {
-              var path = OriginHelper.getBattlefrontPath();
-              if (path.isEmpty) {
-                NotificationService.showNotification(message: 'No executable found.', color: Colors.red);
-                return;
-              }
+          Column(
+            children: [
+              FilledButton(
+                child: const Text('Select Battlefront 2 path manually.'),
+                onPressed: () async {
+                  String? dir = await FilePicker.platform.getDirectoryPath();
+                  if (dir == null) {
+                    return;
+                  }
+                  Directory path = Directory(dir);
+                  if (path.listSync().whereType<File>().where((element) => element.path.endsWith('starwarsbattlefrontii.exe')).isEmpty) {
+                    NotificationService.showNotification(message: 'Could not find Battlefront 2 executable.', color: Colors.red);
+                    return;
+                  }
 
-              Navigator.of(context).pop();
-            },
+                  await box.put('battlefrontPath', dir);
+                  if (!mounted) return;
+                  Navigator.of(context).pop();
+                },
+              ),
+              Button(
+                child: const Text('Try again'),
+                onPressed: () {
+                  var path = OriginHelper.getBattlefrontPath();
+                  if (path.isEmpty) {
+                    NotificationService.showNotification(message: 'No executable found.', color: Colors.red);
+                    return;
+                  }
+
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
           )
         ],
       ),
--- lib/utils/helpers/origin_helper.dart
+++ lib/utils/helpers/origin_helper.dart
@@ -1,9 +1,14 @@
+import 'package:kyber_mod_manager/main.dart';
 import 'package:logging/logging.dart';
 import 'package:win32_registry/win32_registry.dart';
 
 class OriginHelper {
   static String getBattlefrontPath() {
     try {
+      if (box.containsKey('battlefrontPath')) {
+        return box.get('battlefrontPath');
+      }
+
       final ea = Registry.openPath(RegistryHive.localMachine, path: r'SOFTWARE\EA Games\STAR WARS Battlefront II');
       String path = ea.getValueAsString('Install Dir') ?? '';
       return path.substring(0, path.length - 1);
