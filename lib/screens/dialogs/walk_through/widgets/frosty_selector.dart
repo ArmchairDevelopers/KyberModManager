b2d1f20aa91251c9caa52639bdae40941ec3e312
liam <47498574+7reax@users.noreply.github.com>
2022-08-03 15:19:45 +0200

    remove "supported versions" section

--- lib/screens/dialogs/walk_through/widgets/frosty_selector.dart
+++ lib/screens/dialogs/walk_through/widgets/frosty_selector.dart
@@ -1,6 +1,5 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_translate/flutter_translate.dart';
-import 'package:kyber_mod_manager/widgets/unordered_list.dart';
 
 class FrostySelector extends StatelessWidget {
   const FrostySelector({Key? key, required this.supportedVersions}) : super(key: key);
@@ -35,19 +34,19 @@ class FrostySelector extends StatelessWidget {
             fontSize: 16,
           ),
         ),
-        const SizedBox(height: 15),
-        Text(
-          translate('$prefix.supported_versions'),
-          style: const TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        UnorderedList(
-          supportedVersions,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-        )
+        // const SizedBox(height: 15),
+        // Text(
+        //   translate('$prefix.supported_versions'),
+        //   style: const TextStyle(
+        //     fontSize: 16,
+        //     fontWeight: FontWeight.bold,
+        //   ),
+        // ),
+        // UnorderedList(
+        //   supportedVersions,
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   crossAxisAlignment: CrossAxisAlignment.center,
+        // )
       ],
     );
   }
