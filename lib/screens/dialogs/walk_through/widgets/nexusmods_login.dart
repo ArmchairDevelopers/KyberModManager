4381e9df5186ed31438f6179129b73294ce4f1d7
liam <liammatz24@gmail.com>
2022-10-17 21:00:38 +0200

    minor fix

--- lib/screens/dialogs/walk_through/widgets/nexusmods_login.dart
+++ lib/screens/dialogs/walk_through/widgets/nexusmods_login.dart
@@ -68,7 +68,7 @@ class _NexusmodsLoginState extends State<NexusmodsLogin> {
         await box.put('nexusmods_login', true);
         if (!mounted) return;
         Navigator.of(context).pop();
-      } else if (!url.contains('nexusmods')) {
+      } else if (!url.contains('nexusmods') ||! url.startsWith("https://users.nexusmods.com") &&! showOverlay) {
         _controller.loadUrl('https://users.nexusmods.com/auth/sign_in');
       }
     });
@@ -137,7 +137,7 @@ class _NexusmodsLoginState extends State<NexusmodsLogin> {
                                     ),
                                     SizedBox(width: 15),
                                     Text(
-                                      'Loading...',
+                                      'Waiting for NexusMods...',
                                       style: TextStyle(fontSize: 19),
                                     ),
                                   ],
