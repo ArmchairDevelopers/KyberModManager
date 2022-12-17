77ab6992a4f2c02fe5ec2b0fa1058397a3a6dab6
reaxe <47498574+7reax@users.noreply.github.com>
2022-12-17 12:21:29 +0100

    fix

--- lib/screens/run_battlefront/run_dialog.dart
+++ lib/screens/run_battlefront/run_dialog.dart
@@ -1,9 +1,13 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_translate/flutter_translate.dart';
 import 'package:kyber_mod_manager/main.dart';
+import 'package:kyber_mod_manager/utils/helpers/platform_helper.dart';
+import 'package:kyber_mod_manager/utils/services/frosty_profile_service.dart';
 import 'package:kyber_mod_manager/utils/services/frosty_service.dart';
 import 'package:kyber_mod_manager/utils/services/mod_service.dart';
+import 'package:kyber_mod_manager/utils/services/profile_service.dart';
 import 'package:kyber_mod_manager/utils/types/pack_type.dart';
+import 'package:logging/logging.dart';
 
 class RunDialog extends StatefulWidget {
   const RunDialog({Key? key, required this.profile}) : super(key: key);
@@ -35,17 +39,44 @@ class _RunDialogState extends State<RunDialog> {
 
   void startFrosty() async {
     final String profile = selectedProfile.split(' (').first;
-    await ModService.createModPack(
-      packType: getPackType(selectedProfile),
-      profileName: selectedProfile.split(' (').first,
+    final PackType packType = getPackType(selectedProfile);
+    List<dynamic> mods = await ModService.createModPack(
+      context,
+      packType: packType,
+      profileName: profile,
       cosmetics: false,
       onProgress: onProgress,
       setContent: (content) => setState(() => this.content = content),
     );
     if (!mounted) return;
 
-    setState(() => content = translate('$prefix.frosty'));
-    await FrostyService.startFrosty(profile: dynamicEnvEnabled && getPackType(selectedProfile) == PackType.FROSTY_PACK ? profile : null);
+    bool startFrosty = (!dynamicEnvEnabled);
+    if (dynamicEnvEnabled) {
+      if (packType == PackType.FROSTY_PACK) {
+        List<dynamic> appliedMods = await FrostyProfileService.getModsFromProfile(profile);
+        if (!ProfileService.equalModlist(mods, appliedMods)) {
+          Logger.root.info('Mod list is not equal, applying mods');
+          startFrosty = true;
+        }
+      } else if (packType == PackType.MOD_PROFILE || packType == PackType.COSMETICS) {
+        String currentPath = PlatformHelper.getProfile();
+        List<dynamic> activeMods = await FrostyProfileService.getModsFromProfile(currentPath, isPath: true);
+        if (!ProfileService.equalModlist(activeMods, mods) && ProfileService.getSavedProfiles().where((element) => ProfileService.equalModlist(element.mods, mods)).isEmpty) {
+          Logger.root.info("No profile found, starting Frosty");
+          startFrosty = true;
+        }
+      } else if (packType == PackType.NO_MODS) {
+        Logger.root.info("No mods, starting Frosty");
+        startFrosty = true;
+      }
+    }
+
+    if (startFrosty) {
+      setState(() => content = translate('$prefix.frosty'));
+      await FrostyService.startFrosty();
+    } else {
+      PlatformHelper.startBattlefront();
+    }
 
     if (!mounted) return;
     Navigator.of(context).pop();
